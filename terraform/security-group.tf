70f7ba2663e0d85df3a62707349a85de3a74d814
Daniel Manciu <daniel.manciu22@gmail.com>
2025-06-28 20:01:29 +0300

    (terraform) Update market_app_server_sg

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -23,6 +23,22 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv6" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_market_app_server_http_ipv4" {
+  security_group_id = aws_security_group.market_app_server_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_market_app_server_https_ipv4" {
+  security_group_id = aws_security_group.market_app_server_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_market_app_server_ipv4" {
   security_group_id = aws_security_group.market_app_server_sg.id
   cidr_ipv4         = "0.0.0.0/0"
